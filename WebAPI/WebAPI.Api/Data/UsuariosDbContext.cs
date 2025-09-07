cd382d84946bbfe4aa82f072d418587cd0764322
lourenxx <guilhermepexiloureco@gmail.com>
2025-09-07 01:49:00 -0300

    Connecting with DB, Creating a user crud estudie and adding the MVC project to make the FrontEnd

---
+++ WebAPI/WebAPI.Api/Data/UsuariosDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Api.Models;
+
+namespace WebAPI.Api.Data
+{
+    public class UsuariosDbContext : DbContext
+    {
+        
+        public UsuariosDbContext(DbContextOptions<UsuariosDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Usuarios> Usuarios { get; set; } // Tabela para estudos de crud
+    }
+}
