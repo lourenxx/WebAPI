769da99ffbb6ed83fe5ceccde5bad0ecbf0ed1cf
lourenxx <guilhermepexiloureco@gmail.com>
2025-09-12 13:55:12 -0300

    Lista de alunos Api e Web

---
+++ WebAPI/WebAPI.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly Data.UsuariosDbContext _context;
+
+        public UsuarioController(Data.UsuariosDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsuarios()
+        {
+            var usuarios = _context.Usuarios.ToList();
+            return Ok(usuarios);
+        }
+    }
+}
